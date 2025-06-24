f9d0d911867abb3db591f187762f86a5fcefe643
gowtham01raj <gowthamraj.ts@outlook.com>
2025-06-24 08:03:00 +0530

    first

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -2,28 +2,47 @@ import RestaurantCard from "./RestaurantCardList";
 import ResData, { CDN_Link } from "../utils/content";
 import { useState } from "react";
 
-const [searchTxt] = useState();
-const Body = () => (
-  <div className="body">
-    <div className="search-container">
-      <input
-        type="text"
-        className="search-input"
-        placeholder="Search"
-        value={searchTxt}
-        onChange={(e) => {
-          searchTxt = e.target.value;
-        }}
-      />
+function filterData(searchText, restaurants) {
+  const filterData = restaurants.filter((info) =>
+    info.info.name.includes(searchText)
+  );
+  return filterData;
+}
 
-      <button className="search-btn">Search</button>
-    </div>
-    <div className="res-container">
-      {ResData.map((info, index) => {
-        return <RestaurantCard key={index} {...info.info} />;
-      })}
-    </div>
-  </div>
-);
+const Body = () => {
+  const [searchText, setSearchText] = useState(""); //Searchtxt is a local variable. To create a state variable.
+  const [restaurants, setRestaurants] = useState(ResData);
+  return (
+    <>
+      <div className="body">
+        <div className="search-container">
+          <input
+            type="text"
+            className="search-input"
+            placeholder="Search"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            className="search-btn"
+            onClick={() => {
+              const data = filterData(searchText, restaurants);
+              setRestaurants(data);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <div className="res-container">
+          {restaurants.map((info, index) => (
+            <RestaurantCard key={info.info.id} {...info.info} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Body;
